d86ed88afa242b6bfaf59b8866c685fc264d2e81
ruari511 <ruari511@gmail.com>
2018-01-05 23:02:32 +0900

    2018,01.05 공간 몬스터 수 체크

---
+++ GunGame/Assets/Scripts/Stage/Stage_Check.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage_Check : MonoBehaviour {
+	GameObject[] _enemy;
+	public Material m_a;
+	public Material m_b;
+	// Use this for initialization
+	void Start () {
+		m_a = gameObject.GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_enemy = GameObject.FindGameObjectsWithTag("Enemy");
+		if (_enemy.Length == 0)
+		{
+			ChangeColor();
+		}
+
+	}
+	void FixedUpdate()
+	{
+		_enemy = GameObject.FindGameObjectsWithTag("Enemy");
+		if(_enemy.Length==0)
+		{
+			ChangeColor();
+		}
+	}
+
+	void ChangeColor()
+	{
+		gameObject.GetComponent<MeshRenderer>().material = m_b;
+	}
+}
+
